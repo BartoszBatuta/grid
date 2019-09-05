1caeaef3f3686abef155f66e969e265ed538f411
BartoszBatuta <44867490+BartoszBatuta@users.noreply.github.com>
2019-09-05 14:55:05 +0200

    Add files via upload

--- site/main.js
+++ site/main.js
@@ -2,7 +2,7 @@ const items = document.querySelectorAll('.item');
 let color = 0;
 
 for(let i=0;i<items.length;i++){
-    items[i].style.background = `rgb(${color},0 , 255)`;
-    color+=20;
+    items[i].style.background = `rgb(${color},0 , 0)`;
+    color+=11;
     console.log(color);
 }
